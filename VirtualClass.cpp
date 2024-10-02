c3d4396485845f06e74cdfd973a1bdea09e7d2c6
Babukudal <babukudal115@gmail.com>
2024-10-02 19:00:10 +0530

    Virtual class

---
+++ VirtualClass.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+using namespace std;
+ class A
+{
+    public:
+   int a;
+   public:
+   A()
+   {
+      a=1;
+   }
+
+};
+ class B: virtual public A
+{
+    protected:
+   int b;
+
+    public:
+    B()
+    {
+        b=2;
+    }
+   
+
+};
+class C:virtual public A
+{
+    protected:
+    int c;
+    public:
+    C()
+    {
+        c=3;
+    }
+
+};
+class D:public B,public  C
+{
+    protected:
+    int d;
+    public:
+    D()
+    {
+        d=4;
+    }
+    void print()
+    {
+        cout<<A::a<<endl;
+        cout<<B::b<<endl;
+        cout<<C::c<<endl;       
+        cout<<d<<endl;
+    }
+};
+int main( )
+{
+D *d = new D();
+d->print();
+return 0;
+}
+
+
+//here without virtual class inheritance is not possible create object of class D
+//its star inheritance
